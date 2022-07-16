704f837678cbfd86b84b85e32a7de6c485effb7c
Jeon Minji <mjjeon2645@gmail.com>
2022-07-16 15:35:37 +0900

    기본 프레임 준비
    1. MVP 제품 제작을 위한 Swing 기본 요소 준비
    - 프레임 생성
    - 상단 버튼 2개(각각 리뷰보여주기, 리뷰 쓰기 기능을 수행할 예정) 생성
    - 하단에 컨텐츠를 보여 줄 별도의 패널 생성

---
+++ app/src/main/java/BookReview.java
@@ -0,0 +1,72 @@
+import panels.ReviewsPanel;
+import panels.WritePanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BookReview {
+  private JFrame frame;
+  private JPanel menuPanel;
+  private JPanel contentPanel;
+
+  public static void main(String[] args) {
+    BookReview application = new BookReview();
+    application.run();
+  }
+
+  public void run() {
+    initFrame();
+
+    initMenus();
+
+    initContentPanel();
+  }
+
+  public void initFrame() {
+    frame = new JFrame("Book Review 100");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 600);
+    frame.setVisible(true);
+  }
+
+  public void initMenus() {
+    menuPanel = new JPanel();
+    frame.add(menuPanel, BorderLayout.PAGE_START);
+
+    menuPanel.add(displayReviews());
+    menuPanel.add(writeReview());
+  }
+
+  public JButton displayReviews() {
+    JButton button = new JButton("리뷰 보기");
+    button.addActionListener(event -> {
+      ReviewsPanel reviewsPanel = new ReviewsPanel();
+      showContentPanel(reviewsPanel);
+    });
+    return button;
+  }
+
+  public JButton writeReview() {
+    JButton button = new JButton("리뷰 쓰기");
+    button.addActionListener(event -> {
+      WritePanel writePanel = new WritePanel();
+      showContentPanel(writePanel);
+    });
+    return button;
+  }
+
+  public void initContentPanel() {
+    contentPanel = new JPanel();
+    frame.add(contentPanel);
+  }
+
+  public void showContentPanel(JPanel panel) {
+    contentPanel.removeAll();
+    contentPanel.add(panel);
+
+    contentPanel.setVisible(false);
+    contentPanel.setVisible(true);
+
+    frame.setVisible(true);
+  }
+}
