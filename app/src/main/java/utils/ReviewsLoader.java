2f6be74e245a4217f17e6a77393e84c8da479ece
Jeon Minji <mjjeon2645@gmail.com>
2022-07-16 18:46:58 +0900

    csv에 저장되어 있는 review들을 불러오는 ReviewsLoader 생성 및 기능 구현
    1. ReviewsLoader 클래스 생성
    2. 해당 클래스에서 reviewsdata.csv 파일 정보를 가져와 각각 review로 생성
    3. 생성된 review를 리스트에 저장하여 반환시킴

--- app/src/main/java/utils/ReviewsLoader.java
+++ app/src/main/java/utils/ReviewsLoader.java
@@ -2,11 +2,30 @@ package utils;
 
 import models.Review;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ReviewsLoader {
 
-  public List<Review> load() {
-    return null;
+  public List<Review> load() throws FileNotFoundException {
+    List<Review> reviews = new ArrayList<>();
+
+    File file = new File("reviewsdata.csv");
+
+    Scanner scanner = new Scanner(file);
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+
+      String[] words = line.split(",");
+
+      Review review = new Review(words[0], words[1], words[2], words[3]);
+      reviews.add(review);
+    }
+
+    return reviews;
   }
 }
--- app/src/test/java/models/ReviewTest.java
+++ app/src/test/java/models/ReviewTest.java
@@ -12,4 +12,12 @@ class ReviewTest {
         "객체 지향의 사실과 오해 재밌어요",
         "객체지향의사실과오해 어렵지만 재밌네요~");
   }
+
+  @Test
+  void reviewForLoading() {
+    Review review = new Review(
+        "봄이엄마",
+        "객체 지향의 사실과 오해 재밌어요",
+        "객체지향의사실과오해 어렵지만 재밌네요~", "DELETED");
+  }
 }
