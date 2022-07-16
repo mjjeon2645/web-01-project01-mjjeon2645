728673d10aa1ed9e2d88728f94343e215781ecef
Jeon Minji <mjjeon2645@gmail.com>
2022-07-16 16:52:30 +0900

    리뷰 작성 페이지 레이아웃 1차 구성
    1. 타이틀 라벨(리뷰 쓰기)
    2. 리뷰 제목을 입력받는 필드 생성
    3. 리뷰어 이름을 입력받는 필드 생성
    4. 100자 리뷰를 입력받는 텍스트area 생성
    5. 작성완료 버튼 생성

--- app/src/main/java/panels/ReviewsPanel.java
+++ app/src/main/java/panels/ReviewsPanel.java
@@ -3,4 +3,8 @@ package panels;
 import javax.swing.*;
 
 public class ReviewsPanel extends JPanel {
+  public ReviewsPanel() {
+    JLabel titleLabel = new JLabel("리뷰 목록");
+    this.add(titleLabel);
+  }
 }
